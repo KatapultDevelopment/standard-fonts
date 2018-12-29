00986cb1f5fee0d56fa80e8bbb231280c6eae18d
Andrew Dillon <andrew.dillon.j@gmail.com>
2018-12-29 17:29:57 -0600

    Change array to string conversion method

--- src/utils.ts
+++ src/utils.ts
@@ -1,10 +1,8 @@
 import * as base64 from 'base64-arraybuffer';
 import pako from 'pako';
 
-export const decompressJson = (compressedJson: string): string => {
-  const json = String.fromCharCode.apply(
-    String,
-    pako.inflate(base64.decode(compressedJson)),
-  );
-  return json;
-};
+const arrayToString = (array: Uint8Array) =>
+  array.reduce((data, byte) => data + String.fromCharCode(byte), '');
+
+export const decompressJson = (compressedJson: string): string =>
+  arrayToString(pako.inflate(base64.decode(compressedJson)));
