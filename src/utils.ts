461cc6df00385e0506a9407de556204596e8386f
Andrew Dillon <andrew.dillon.j@gmail.com>
2018-12-29 18:23:58 -0600

    Bump version to 0.0.4

--- src/utils.ts
+++ src/utils.ts
@@ -1,8 +1,13 @@
 import * as base64 from 'base64-arraybuffer';
 import pako from 'pako';
 
-const arrayToString = (array: Uint8Array) =>
-  array.reduce((data, byte) => data + String.fromCharCode(byte), '');
+const arrayToString = (array: Uint8Array) => {
+  let str = '';
+  for (let i = 0; i < array.length; i++) {
+    str += String.fromCharCode(array[i]);
+  }
+  return str;
+};
 
 export const decompressJson = (compressedJson: string): string =>
   arrayToString(pako.inflate(base64.decode(compressedJson)));
